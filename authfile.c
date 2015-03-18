517507ce020c0015e01e6060e143b4aa7ffc217c
djm@openbsd.org <djm@openbsd.org>
2015-03-18 01:44:21 +0000

    upstream commit
    KRL support doesn't need OpenSSL anymore, remove #ifdefs
     from around call

--- authfile.c
+++ authfile.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: authfile.c,v 1.116 2015/07/09 09:49:46 markus Exp $ */
+/* $OpenBSD: authfile.c,v 1.112 2015/03/18 01:44:21 djm Exp $ */
 /*
  * Copyright (c) 2000, 2013 Markus Friedl.  All rights reserved.
  *
