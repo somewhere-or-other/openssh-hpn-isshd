ede308f5a95f80bfbc7584297dffacc7d01e404b
Damien Miller <djm@mindrot.org>
2015-05-05 19:10:58 +1000

    upstream commit
       - tedu@cvs.openbsd.org 2015/01/12 03:20:04
         [bcrypt_pbkdf.c]
         rename blocks to words. bcrypt "blocks" are unrelated to blowfish blocks,
         nor are they the same size.

--- openbsd-compat/bcrypt_pbkdf.c
+++ openbsd-compat/bcrypt_pbkdf.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: bcrypt_pbkdf.c,v 1.12 2015/01/08 00:30:07 deraadt Exp $ */
+/* $OpenBSD: bcrypt_pbkdf.c,v 1.13 2015/01/12 03:20:04 tedu Exp $ */
 /*
  * Copyright (c) 2013 Ted Unangst <tedu@openbsd.org>
  *
