fe6d26cb66cce59bb86a0b5ed040cd59c5ef9b5b
djm@openbsd.org <djm@openbsd.org>
2015-05-22 04:45:52 +0000

    upstream commit
    Reorder EscapeChar option parsing to avoid a single-byte
     out- of-bounds read. bz#2396 from Jaak Ristioja; ok dtucker@
    
    Upstream-ID: 1dc6b5b63d1c8d9a88619da0b27ade461d79b060

--- readconf.c
+++ readconf.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: readconf.c,v 1.235 2015/05/04 06:10:48 djm Exp $ */
+/* $OpenBSD: readconf.c,v 1.236 2015/05/22 04:45:52 djm Exp $ */
 /*
  * Author: Tatu Ylonen <ylo@cs.hut.fi>
  * Copyright (c) 1995 Tatu Ylonen <ylo@cs.hut.fi>, Espoo, Finland
