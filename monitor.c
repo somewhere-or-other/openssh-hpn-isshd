57a3ddb3584b1456cd64d962532d4c8b526ca49c
djm@openbsd.org <djm@openbsd.org>
2015-06-22 23:42:16 +0000

    upstream commit
    Don't count successful partial authentication as failures
     in monitor; this may have caused the monitor to refuse multiple
     authentications that would otherwise have successfully completed; ok markus@
    
    Upstream-ID: eb74b8e506714d0f649bd5c300f762a527af04a3

--- monitor.c
+++ monitor.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: monitor.c,v 1.149 2015/05/04 06:10:48 djm Exp $ */
+/* $OpenBSD: monitor.c,v 1.150 2015/06/22 23:42:16 djm Exp $ */
 /*
  * Copyright 2002 Niels Provos <provos@citi.umich.edu>
  * Copyright 2002 Markus Friedl <markus@openbsd.org>
