2d96d5520784c1cfae02ab2c6110e5367e78bb89
djm@openbsd.org <djm@openbsd.org>
2015-06-29 22:35:12 +0000

    upstream commit
    add getpid to sandbox, reachable by grace_alarm_handler
    
    reported by Jakub Jelen; bz#2419
    
    Upstream-ID: d0da1117c16d4c223954995d35b0f47c8f684cd8

--- sandbox-systrace.c
+++ sandbox-systrace.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: sandbox-systrace.c,v 1.15 2015/05/18 15:06:05 deraadt Exp $ */
+/* $OpenBSD: sandbox-systrace.c,v 1.16 2015/06/29 22:35:12 djm Exp $ */
 /*
  * Copyright (c) 2011 Damien Miller <djm@mindrot.org>
  *
