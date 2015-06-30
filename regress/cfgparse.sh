8fc4153fc1a31f5b5b45652f333aa60e04675465
Damien Miller <djm@mindrot.org>
2015-06-30 16:05:40 +1000

    skip IPv6-related portions on hosts without IPv6
    with Tim Rice

--- regress/cfgparse.sh
+++ regress/cfgparse.sh
@@ -3,6 +3,11 @@
 
 tid="config parse"
 
+# This is a reasonable proxy for IPv6 support.
+if ! config_defined HAVE_STRUCT_IN6_ADDR ; then
+	SKIP_IPV6=yes
+fi
+
 # We need to use the keys generated for the regression test because sshd -T
 # will fail if we're not running with SUDO (no permissions for real keys) or
 # if we are # running tests on a system that has never had sshd installed
@@ -31,6 +36,7 @@ EOD
 listenaddress [::1]:1234
 listenaddress [::1]:5678
 EOD
+
 # test input sets.  should all result in the output above.
 # test 1: addressfamily and port first
 cat > $OBJ/sshd_config.1 <<EOD
@@ -52,7 +58,6 @@ EOD
 cat > $OBJ/sshd_config.1 <<EOD
 ${SSHD_KEYS}
 listenaddress 1.2.3.4
-listenaddress ::1
 port 1234
 port 5678
 addressfamily any
