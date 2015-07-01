84986e3bcb09ade698138298a84dd723f9346c3c
djm@openbsd.org <djm@openbsd.org>
2015-07-01 01:55:13 +0000

    upstream commit
    openssh-6.9
    
    Upstream-ID: 6cfe8e1904812531080e6ab6e752d7001b5b2d45

--- version.h
+++ version.h
@@ -1,6 +1,5 @@
-/* $OpenBSD: version.h,v 1.75 2015/08/21 03:45:26 djm Exp $ */
-
-#define SSH_VERSION	"OpenSSH_7.1"
+/* $OpenBSD: version.h,v 1.73 2015/07/01 01:55:13 djm Exp $ */
+#define SSH_VERSION	"OpenSSH_6.9"
 
 #define SSH_PORTABLE	"p2"
 #define SSH_HPN         "-hpn14v9"
