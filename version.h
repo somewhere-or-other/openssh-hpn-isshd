dbe7f8d99c875ec7937820a924dd7b8acfca8bf7
scott campbell <set.element@gmail.com>
2016-01-25 07:56:53 -0500

    bump version

--- version.h
+++ version.h
@@ -2,12 +2,12 @@
 
 #define SSH_VERSION	"OpenSSH_7.1"
 
-#define SSH_PORTABLE	"p1"
-#define SSH_HPN         "-hpn14v9"
+#define SSH_PORTABLE	"p2"
+#define SSH_HPN         "-hpn14v10"
 #define SSH_RELEASE	SSH_VERSION SSH_PORTABLE SSH_HPN
 
 #ifdef NERSC_MOD
 #undef SSH_RELEASE
-#define SSH_AUDITING	"NMOD_3.17"
+#define SSH_AUDITING	"NMOD_3.19"
 #define SSH_RELEASE	SSH_VERSION SSH_PORTABLE SSH_HPN SSH_AUDITING
 #endif /* NERSC_MOD */
