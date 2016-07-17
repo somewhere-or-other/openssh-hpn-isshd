1f8bceb1a404b8961ddd788c6514061fb8257809
scott campbell <set.element@gmail.com>
2016-07-17 12:00:04 -0400

    fix version

--- version.h
+++ version.h
@@ -1,9 +1,9 @@
 /* $OpenBSD: version.h,v 1.75 2015/08/21 03:45:26 djm Exp $ */
 
-#define SSH_VERSION	"OpenSSH_7.1"
+#define SSH_VERSION	"OpenSSH_7.2"
 
 #define SSH_PORTABLE	"p2"
-#define SSH_HPN         "-hpn14v10"
+#define SSH_HPN         "-hpn14v11"
 #define SSH_RELEASE	SSH_VERSION SSH_PORTABLE SSH_HPN
 
 #ifdef NERSC_MOD
