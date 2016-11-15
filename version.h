7c39ac596b20a75fed6ef6231b2216add33e4b59
Venkat Yekkirala <ysvenkat@ncsa.illinois.edu>
2016-11-15 11:44:09 -0600

    Fixed conflict.

--- version.h
+++ version.h
@@ -6,11 +6,8 @@
 #define SSH_HPN         "-hpn14v11"
 #define SSH_RELEASE	SSH_VERSION SSH_PORTABLE SSH_HPN
 
-<<<<<<< HEAD
 #ifdef NERSC_MOD
 #undef SSH_RELEASE
 #define SSH_AUDITING	"NMOD_3.19"
 #define SSH_RELEASE	SSH_VERSION SSH_PORTABLE SSH_HPN SSH_AUDITING
 #endif /* NERSC_MOD */
-=======
->>>>>>> master
