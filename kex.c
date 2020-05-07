999c0339a288b8f7f2dcac4889d4534b247521ba
Scott Campbell <scampbell@potato.local>
2020-05-07 14:34:26 -0700

    Why: Port to 8.x; change in kex struct requires slight name changes
    *
    
    This change addresses the need by:
    
    *

--- kex.c
+++ kex.c
@@ -994,7 +994,7 @@ kex_choose_conf(struct ssh *ssh)
 			/* t4buf is not derived from authlen directly, but in  */
 			/*  the event that that value is borked, play it safe  */
 			/*  and bail on this as well cause paranoid ...        */
-                        char* t4buf = ((authlen != 0 || !kex->client_version_string) ? (encode_string(def_vers_string, sizeof(def_vers_string)-1)) : (encode_string(kex->client_version_string, strlen(kex->client_version_string))));
+                        char* t4buf = ((authlen != 0 || !kex->client_version) ? (encode_string(def_vers_string, sizeof(def_vers_string)-1)) : (encode_string(kex->client_version->cd, strlen(kex->client_version->cd))));
 
 			s_audit("session_key_exchange", "count=%i uristring=%s_%s_%s_%s",
 			client_session_id, t4buf, t1buf, t2buf, t3buf);
